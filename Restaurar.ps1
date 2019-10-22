fa2ebf101487f8a087d47170aaad8e5281b8e24b
Ivo Dias <igd753@gmail.com>
2019-10-22 12:23:57 -0300

    Update

--- Restaurar.ps1
+++ Restaurar.ps1
@@ -1,6 +1,28 @@
 # Fazer Backup do SQL
 # Ivo Dias
 
+# Funcao para pegar a pasta
+Function Get-Folder($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    $OpenFileDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    $OpenFileDialog.rootfolder = "MyComputer"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $caminhoPasta = $OpenFileDialog.SelectedPath
+    return $caminhoPasta
+}
+
+# Funcao para pegar arquivo
+Function Get-File($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "Inputs (*.inp)| *.inp"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
+
 # Formulario principal
 Add-Type -assembly System.Windows.Forms # Recebe a biblioteca
 $GUI = New-Object System.Windows.Forms.Form # Cria o formulario principal
@@ -56,43 +78,35 @@ $txtSenha.Width = 200 # Configura o tamanho
 $txtSenha.Location  = New-Object System.Drawing.Point(100,70) # Define em qual coordenada da tela vai ser desenhado
 $GUI.Controls.Add($txtSenha) # Adiciona ao formulario principal
 
-# Procedimento
-$lblProcedimento = New-Object System.Windows.Forms.Label # Cria a label
-$lblProcedimento.Text = "Procedimento:" # Define um texto para ela
-$lblProcedimento.Location  = New-Object System.Drawing.Point(0,90) # Define em qual coordenada da tela vai ser desenhado
-$lblProcedimento.AutoSize = $true # Configura tamanho automatico
-$GUI.Controls.Add($lblProcedimento) # Adiciona ao formulario principal
-$cbxProcedimento = New-Object System.Windows.Forms.ComboBox # Cria uma Combobox
-$cbxProcedimento.Width = 200 # Define um tamanho
-$cbxProcedimento.Location  = New-Object System.Drawing.Point(100,90) # Define a localizacao
-$cbxProcedimento.Items.Add("Backup") # Exemplo de opcoes 
-$cbxProcedimento.Items.Add("Restore") # Exemplo de opcoes
-$GUI.Controls.Add($cbxProcedimento) # Adiciona ao formulario principal
-
 # Diretorio
 $lblDiretorio = New-Object System.Windows.Forms.Label # Cria a label
-$lblDiretorio.Text = "Diretorio:" # Define um texto para ela
+$lblDiretorio.Text = "Diretorio Backup:" # Define um texto para ela
 $lblDiretorio.Location  = New-Object System.Drawing.Point(0,110) # Define em qual coordenada da tela vai ser desenhado
 $lblDiretorio.AutoSize = $true # Configura tamanho automatico
+$lblDiretorio.Visible = $false # Deixa invisivel na inicializacao
 $GUI.Controls.Add($lblDiretorio) # Adiciona ao formulario principal
-
-# Funcao para pegar o caminho do arquivo
-Function Get-Folder($initialDirectory)
-{
-    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-    $OpenFileDialog = New-Object System.Windows.Forms.FolderBrowserDialog
-    $OpenFileDialog.rootfolder = "MyComputer"
-    $OpenFileDialog.ShowDialog() | Out-Null
-    $caminhoPasta = $OpenFileDialog.SelectedPath
-    return $caminhoPasta
-}
-
 # Botao para escolher o arquivo
 $btnDiretorio = New-Object System.Windows.Forms.Button # Cria um botao
 $btnDiretorio.Location = New-Object System.Drawing.Size(100,110) # Define em qual coordenada da tela vai ser desenhado
 $btnDiretorio.Size = New-Object System.Drawing.Size(200,18) # Define o tamanho
+$btnDiretorio.visible = $false # Deixa invisivel na inicializacao
 $btnDiretorio.Text = "Definir pasta" # Define o texto
 $GUI.Controls.Add($btnDiretorio) # Adiciona ao formulario principal
+
+# Arquivo
+$lblArquivo = New-Object System.Windows.Forms.Label # Cria a label
+$lblArquivo.Text = "Arquivo Restore:" # Define um texto para ela
+$lblArquivo.Location  = New-Object System.Drawing.Point(0,110) # Define em qual coordenada da tela vai ser desenhado
+$lblArquivo.AutoSize = $true # Configura tamanho automatico
+$lblArquivo.Visible = $false # Deixa invisivel na inicializacao
+$GUI.Controls.Add($lblArquivo) # Adiciona ao formulario principal
+# Botao para escolher o arquivo
+$btnArquivo = New-Object System.Windows.Forms.Button # Cria um botao
+$btnArquivo.Location = New-Object System.Drawing.Size(100,110) # Define em qual coordenada da tela vai ser desenhado
+$btnArquivo.Size = New-Object System.Drawing.Size(200,18) # Define o tamanho
+$btnArquivo.visible = $false # Deixa invisivel na inicializacao
+$btnArquivo.Text = "Escolher arquivo" # Define o texto
+$GUI.Controls.Add($btnArquivo) # Adiciona ao formulario principal
 
 # Botao para fazer o procedimento
 $btnSQL = New-Object System.Windows.Forms.Button # Cria um botao
@@ -101,16 +115,58 @@ $btnSQL.Size = New-Object System.Drawing.Size(300,25) # Define o tamanho
 $btnSQL.Text = "Fazer" # Define o texto
 $GUI.Controls.Add($btnSQL) # Adiciona ao formulario principal
 
+# Procedimento
+$lblProcedimento = New-Object System.Windows.Forms.Label # Cria a label
+$lblProcedimento.Text = "Procedimento:" # Define um texto para ela
+$lblProcedimento.Location  = New-Object System.Drawing.Point(0,90) # Define em qual coordenada da tela vai ser desenhado
+$lblProcedimento.AutoSize = $true # Configura tamanho automatico
+$GUI.Controls.Add($lblProcedimento) # Adiciona ao formulario principal
+# Botao para escolher o procedimento
+# Backup:
+$btnBackup = New-Object System.Windows.Forms.Button # Cria um botao
+$btnBackup.Location = New-Object System.Drawing.Size(100,90) # Define em qual coordenada da tela vai ser desenhado
+$btnBackup.Size = New-Object System.Drawing.Size(100,18) # Define o tamanho
+$btnBackup.Text = "Backup" # Define o texto
+$GUI.Controls.Add($btnBackup) # Adiciona ao formulario principal
+# Restore:
+$btnRestore = New-Object System.Windows.Forms.Button # Cria um botao
+$btnRestore.Location = New-Object System.Drawing.Size(200,90) # Define em qual coordenada da tela vai ser desenhado
+$btnRestore.Size = New-Object System.Drawing.Size(100,18) # Define o tamanho
+$btnRestore.Text = "Restore" # Define o texto
+$GUI.Controls.Add($btnRestore) # Adiciona ao formulario principal
+
 # Label para receber o retorno do procedimento
 $lblResposta = New-Object System.Windows.Forms.Label # Cria a label
 $lblResposta.Text = "" # Coloca um texto em branco
-$lblResposta.Location  = New-Object System.Drawing.Point(0,150) # Define em qual coordenada da tela vai ser desenhado
+$lblResposta.Location  = New-Object System.Drawing.Point(0,190) # Define em qual coordenada da tela vai ser desenhado
 $lblResposta.AutoSize = $true # Configura tamanho automatico
 $GUI.Controls.Add($lblResposta) # Adiciona ao formulario principal
+
+# Evento do Botao Arquivo
+$btnRestore.Add_Click({
+    $lblDiretorio.Visible = $false # Deixa invisivel
+    $btnDiretorio.visible = $false # Deixa invisivel
+    $lblArquivo.Visible = $true # Deixa visivel
+    $btnArquivo.visible = $true # Deixa visivel
+})
+
+# Evento do Botao Backup
+$btnBackup.Add_Click({
+    $lblArquivo.Visible = $false # Deixa invisivel
+    $btnArquivo.visible = $false # Deixa invisivel
+    $lblDiretorio.Visible = $true # Deixa visivel
+    $btnDiretorio.visible = $true # Deixa visivel
+})
 
 # Evento do Botao Diretorio
 $btnDiretorio.Add_Click({
     $caminhoPasta = Get-Folder # Recebe a pasta onde o backup precisa ser feito
+    $lblResposta.Text = $caminhoPasta
+})
+
+# Evento do Botao Restore
+$btnArquivo.Add_Click({
+    $caminhoPasta = Get-File # Recebe a pasta onde o backup precisa ser feito
     $lblResposta.Text = $caminhoPasta
 })
 
